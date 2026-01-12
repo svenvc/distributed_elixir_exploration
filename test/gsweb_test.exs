8d643fb38801f712ae0eff03c9b7d461c8e92cc2
Sven Van Caekenberghe <sven@stfx.eu>
2026-01-12 18:21:14 +0100

    test source code formatting

--- test/gsweb_test.exs
+++ test/gsweb_test.exs
@@ -10,28 +10,39 @@ defmodule GswebTest do
       Enum.member?(Req.post!("http://localhost:4000/process/kv1/call", json: "keys").body, "foo")
     )
 
-    assert(Req.post!("http://localhost:4000/process/kv1/call", json: ["error"]).body == "not_implemented")
+    assert(
+      Req.post!("http://localhost:4000/process/kv1/call", json: ["error"]).body ==
+        "not_implemented"
+    )
   end
 
   test "pub-sub genserver" do
-    Process.spawn(fn ->
-      Process.sleep(100)
-      assert(
-        Req.post!("http://localhost:4000/process/ps1/call", json: ["broadcast", "topic1", ["msg", 101]]).body
-      )
-    end, [])
+    Process.spawn(
+      fn ->
+        Process.sleep(100)
+
+        assert(
+          Req.post!("http://localhost:4000/process/ps1/call",
+            json: ["broadcast", "topic1", ["msg", 101]]
+          ).body
+        )
+      end,
+      []
+    )
 
     Req.post!(
       "http://localhost:4000/process/ps1/call-receive",
       json: ["subscribe", "topic1"],
       into: fn {:data, data}, {req, resp} ->
         Logger.debug("received: #{data}")
-          if (String.starts_with?(data, "data:")) do
-            assert(String.slice(data, 6..-1//1) |> JSON.decode!() == ["msg", 101])
-            {:halt, {req, resp}}
-          else
-            {:cont, {req, resp}}
-          end
-      end)
+
+        if String.starts_with?(data, "data:") do
+          assert(String.slice(data, 6..-1//1) |> JSON.decode!() == ["msg", 101])
+          {:halt, {req, resp}}
+        else
+          {:cont, {req, resp}}
+        end
+      end
+    )
   end
 end
